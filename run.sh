36d21d4cec756d976bf4c797c8c598abdb84c815
Sayapin Alexander <wizardjedi@gmail.com>
2012-03-03 23:55:54 +0400

    Added command line argument to specify logging directory

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-erl -pa ebin -run dummy_smsc start -smsc system_id TEST -smsc password TEST
+erl -pa ebin -config sys_config.config -run dummy_smsc start -smsc system_id TEST -smsc password TEST -smsc log log
--- src/dummy_smsc.erl
+++ src/dummy_smsc.erl
@@ -5,6 +5,14 @@
 start()->
 	application:start(sasl),
 	application:start(log4erl),
-	log4erl:add_file_appender(file,{"log", "dummy_smsc", {size, 100000}, 4, "elog", debug}),
+	{ok, Args} = init:get_argument(smsc),
+	[Log] = [[Y] || ["log",Y] <- Args],
+	case Log of
+		[LogDir] ->
+			LogDir;
+		_ ->
+			LogDir = "log"
+	end,
+	log4erl:add_file_appender(file,{LogDir, "dummy_smsc", {size, 100000}, 4, "elog", debug}),
 	log4erl:change_format(file, "%Y-%M-%D %T [%L] %l%n"),
 	application:start(dummy_smsc).
